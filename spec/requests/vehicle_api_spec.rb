32482a4099dde3347f58e88df435cea87239d06f
fatma <contactfatma@gmail.com>
2018-01-31 20:09:53 -0800

    add rspec tests for negative case when vehicle_id is zero and requests for security status

--- spec/requests/vehicle_api_spec.rb
+++ spec/requests/vehicle_api_spec.rb
@@ -12,4 +12,15 @@ describe 'vehicle api', type: :request do
       }.to_json
     )
   end
+
+  it 'handles empty response from GM' do
+    get '/vehicles/0'
+    expect(response.status).to eq 404
+  end
+
+  it 'requests vehicle security status' do
+    get '/vehicles/1234/doors'
+    expect(JSON.parse(response.body).first['location']).to be_kind_of(String)
+    expect(JSON.parse(response.body).first['locked']).to be_in([true, false])
+  end
 end
