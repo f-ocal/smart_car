1725382c79d35aced8cadad79c6452073cd1421a
fatma <contactfatma@gmail.com>
2018-02-01 00:16:23 -0800

    add integration tests for start and stop engine

--- spec/requests/vehicle_api_spec.rb
+++ spec/requests/vehicle_api_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 describe 'vehicle api', type: :request do
-  it 'makes a request to GM and formats the response' do
+  #TODO: integration tests for making request to smart car api and return formatted response
+  it 'requests vehicle information' do
     get '/vehicles/1234'
     expect(response.body).to eq(
       {
@@ -24,13 +25,34 @@ describe 'vehicle api', type: :request do
     expect(JSON.parse(response.body).first['locked']).to be_in([true, false])
   end
 
-  it 'requests vehicle fuel status' do
-    get '/vehicles/1234/fuel'
-    expect(JSON.parse(response.body)['percent']).to be_kind_of(Integer)
+  describe 'returning vehicle fuel and battery status' do
+    it 'requests vehicle fuel status' do
+      get '/vehicles/1234/fuel'
+      expect(JSON.parse(response.body)['percent']).to be_kind_of(Integer)
+    end
+
+    it 'requests vehicle battery status' do
+      get '/vehicles/1235/battery'
+      expect(response).to have_http_status(200)
+      expect(JSON.parse(response.body)['percent']).to be_kind_of(Integer)
+    end
   end
 
-  it 'requests vehicle battery status' do
-    get '/vehicles/1235/battery'
-    expect(JSON.parse(response.body)['percent']).to be_kind_of(Integer)
+  describe 'starting and stopping the vehicle' do
+    it 'requests to start the vehicle' do
+      params = { "action" => "START" }
+      headers = { "CONTENT_TYPE" => "application/json" }
+      expect(GMApiClient).to receive(:start_stop_engine).with('1234', 'START').and_call_original
+      post '/vehicles/1234/engine', params: params.to_json, headers: headers
+      expect(JSON.parse(response.body)['status']).to be_in(["success", "error"])
+    end
+
+    it 'requests to stop the vehicle' do
+      params = { "action" => "STOP" }
+      headers = { "CONTENT_TYPE" => "application/json" }
+      expect(GMApiClient).to receive(:start_stop_engine).with('1234', 'STOP').and_call_original
+      post '/vehicles/1234/engine', params: params.to_json, headers: headers
+      expect(JSON.parse(response.body)['status']).to be_in(["success", "error"])
+    end
   end
 end
