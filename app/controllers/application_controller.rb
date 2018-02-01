44d68eb910a0adcd5f9a18e77e87dc64db016239
fatma <contactfatma@gmail.com>
2018-02-01 00:58:36 -0800

    protect from forgery with null session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # protect_from_forgery with: :null_session #use this to POST request on simple rest client or postman
 end
