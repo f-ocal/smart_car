5d715a7daf7eaffabd0f6f2c078e687e136be591
fatma <contactfatma@gmail.com>
2018-02-01 00:17:46 -0800

    add engine to vehicle routes and add engine controller with create method

---
+++ app/controllers/engine_controller.rb
@@ -0,0 +1,10 @@
+class EngineController < ApplicationController
+  def create
+    start_stop_engine_status = GMApiClient.start_stop_engine(params[:vehicle_id], params[:engine][:action])
+    if start_stop_engine_status['status'] == '200'
+      render json: VehicleInfoPresenter.new.format_start_engine_info(start_stop_engine_status)
+    else
+      render json: {}, status: 400
+    end
+  end
+end
