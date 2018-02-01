1646e0c00491ac40586ede05bc54d695eba87079
fatma <contactfatma@gmail.com>
2018-01-31 20:07:43 -0800

    rspec tests for GMApiClient to get vehicle information and security status

--- spec/api_clients/gm_api_client_spec.rb
+++ spec/api_clients/gm_api_client_spec.rb
@@ -1,11 +1,48 @@
 require 'rails_helper'
 
 describe GMApiClient do
-  it 'makes a get to the GM api' do
+  it 'gets vehicle info' do
+    #TODO: this test hits the gm api and expects a specific response. Rewrite test to not expect exact response
     id = 1234
     response = GMApiClient.get_vehicle_info(id)
 
-    expect(response).to eq %({"service":"getVehicleInfo","status":"200","data":{"vin":{"type":"String","value":"123123412412"},"color":{"type":"String","value":"Metallic Silver"},"fourDoorSedan":{"type":"Boolean","value":"True"},"twoDoorCoupe":{"type":"Boolean","value":"False"},"driveTrain":{"type":"String","value":"v8"}}})
+    expected_response = {
+      "service"=>"getVehicleInfo",
+      "status"=>"200",
+      "data"=>{
+        "vin"=>{
+          "type"=>"String",
+          "value"=>"123123412412"
+        },
+        "color"=>{
+          "type"=>"String",
+          "value"=>"Metallic Silver"
+        },
+        "fourDoorSedan"=>{
+          "type"=>"Boolean",
+          "value"=>"True"
+        },
+        "twoDoorCoupe"=>{
+          "type"=>"Boolean",
+          "value"=>"False"
+        },
+        "driveTrain"=>{
+          "type"=>"String",
+          "value"=>"v8"
+        }
+      }
+    }
+    expect(response).to eq expected_response
+
+  end
+
+  it 'gets vehicle security status' do
+    id = 1234
+    response = GMApiClient.get_security_status(id)
+
+    expected_locations = ['backRight', 'frontRight', 'backLeft', 'frontLeft']
+    expect(expected_locations).to include response['data']['doors']['values'].first['location']['value']
+    expect(['True', 'False']).to include response['data']['doors']['values'].first['locked']['value']
   end
 
 end
