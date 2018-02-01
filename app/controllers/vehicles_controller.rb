3b396d7655fe8c8c98da7e373987e860b590f301
fatma <contactfatma@gmail.com>
2018-01-31 20:13:02 -0800

    add vehicles controller to make requests to GMApiClient and Vehicle Info Presenter

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,7 +1,11 @@
 class VehiclesController < ApplicationController
 
   def show
-    render json: {}
+    vehicle_info = GMApiClient.get_vehicle_info(params[:id])
+    if vehicle_info['status'] == '200'
+      render json: VehicleInfoPresenter.new.format_vehicle_info(vehicle_info)
+    else
+      render json: {}, status: 404
+    end
   end
-
 end
